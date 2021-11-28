774de83d213380c50899b2f9c5c511b9f4e8dbbd
Muhammad Tahir <m.tahiridrees27@gmail.com>
2021-11-28 10:12:12 +0500

    Classes in OPP

--- lecture/ES6/27-11-2021/28-11-2021/custom.js
+++ lecture/ES6/27-11-2021/28-11-2021/custom.js
@@ -40,3 +40,29 @@
 // console.log(obj.changeName());
 // console.log(obj.a.changeName());
 // // Global Object
+
+
+
+// Classes In OOP
+// Class is a blue print to make object
+
+
+// Class of Human
+
+class Human {
+    // Constructor -> function
+    constructor(name, height, age) {
+        this.name = name;
+        this.height = height;
+        this.age = age;
+    };
+    // name, Heght, age
+    // Change Name, Height, Age
+};
+
+const mySelf = new Human("Muhammad Tahir", '5.8', '20');
+const mySelf2 = new Human("Syed Husnain Shah", '5.8', '20');
+const mySelf3 = new Human("Hamza Amir", '5.8', '20');
+console.log("My Self: ", mySelf);
+console.log("My Self2: ", mySelf2);
+console.log("My Self3: ", mySelf3);
