127577f5968c754bac50bd5bc6f7328841d4f2e5
Muhammad Tahir <m.tahiridrees27@gmail.com>
2021-12-11 10:17:52 +0500

    Fatch Api

--- lecture/ES6/05-12-2021/custom.js
+++ lecture/ES6/05-12-2021/custom.js
@@ -1,3 +1,12 @@
+// Fetch API
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (json) {
+        console.log(json);
+    })
+
 let myPromise = new Promise(function (resolve, reject) {
     let connection = false;
     if (connection) {
