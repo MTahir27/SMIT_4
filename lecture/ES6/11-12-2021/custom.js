2fbcc3a05086450a67104929d7639676e2303975
Muhammad Tahir <m.tahiridrees27@gmail.com>
2021-12-11 10:33:10 +0500

    Promise Concept

--- lecture/ES6/11-12-2021/custom.js
+++ lecture/ES6/11-12-2021/custom.js
@@ -1,8 +1,16 @@
 const promise1 = new Promise(function (resolve, reject) {
     setTimeout(() => {
         resolve();
-        reject();
     }, 3000);
+
+
+    const arr = [];
+    for (let index = 0; index < 1000; index++) {
+        console.log("Task in Progress");
+        arr.push(index);
+    }
+    console.log(arr);
+
 });
 
 console.log(1);
@@ -13,9 +21,4 @@ promise1.then(function () {
     console.log("Function Reject");
 });
 
-const arr = [];
-for (let index = 0; index < 1000; index++) {
-    console.log("Task in Progress");
-    arr.push(index);
-}
-console.log(arr);
+console.log(2);
