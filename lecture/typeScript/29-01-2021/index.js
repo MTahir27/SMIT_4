a032f59a417573bd8c4977db682381308d673c8b
MTahir27 <m.tahiridrees27@gmail.com>
2022-01-29 14:31:14 +0500

    Type Scrpt

---
+++ lecture/typeScript/29-01-2021/index.js
@@ -0,0 +1,8 @@
+console.log("Hello");
+console.log("Again Test");
+console.log("Hello Again");
+// ABsb
+function greet(person, date) {
+    console.log("Hello ".concat(person, ", today is ").concat(date, "!"));
+}
+greet("Brendan", "abc");
---
+++ lecture/typeScript/29-01-2021/index.ts
@@ -0,0 +1,8 @@
+console.log("Hello");
+console.log("Again Test");
+console.log("Hello Again");
+// ABsb
+function greet(person: string, date: string) {
+  console.log(`Hello ${person}, today is ${date}!`);
+}
+greet("Brendan", "abc");
