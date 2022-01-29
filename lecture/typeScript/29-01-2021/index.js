6b148a3074fe5c68bea40686eb6a9542cc50c59b
MTahir27 <m.tahiridrees27@gmail.com>
2022-01-29 14:42:38 +0500

    Data Type Decleartion

--- lecture/typeScript/29-01-2021/index.js
+++ lecture/typeScript/29-01-2021/index.js
@@ -1,8 +1,13 @@
-console.log("Hello");
-console.log("Again Test");
-console.log("Hello Again");
-// ABsb
+// Data Type Decleration
 function greet(person, date) {
     console.log("Hello ".concat(person, ", today is ").concat(date, "!"));
 }
 greet("1", new Date("12-12-2020"));
+// String
+var name1 = "Tahir";
+// Number
+var number = 1;
+// Array
+var array = ["One", "Two"];
+// Any
+var anyDataType = "234";
--- lecture/typeScript/29-01-2021/index.ts
+++ lecture/typeScript/29-01-2021/index.ts
@@ -1,8 +1,14 @@
-console.log("Hello");
-console.log("Again Test");
-console.log("Hello Again");
-// ABsb
+// Data Type Decleration
 function greet(person: string, date: Date) {
   console.log(`Hello ${person}, today is ${date}!`);
 }
 greet("1", new Date("12-12-2020"));
+
+// String
+const name1: string = "Tahir";
+// Number
+const number: number = 1;
+// Array
+const array: string[] = ["One", "Two"];
+// Any
+const anyDataType: any = "234";
